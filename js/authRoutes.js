176041be5de36c73a235de26b4cf511418bf48f7
PedroMAnjos <pedromestredosanjos@gmail.com>
2026-05-18 02:04:36 -0300

    Delete js directory

--- js/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/login', AuthController.login);
-router.post('/validate-hash', authMiddleware, AuthController.validateHash);
-
-module.exports = router;
