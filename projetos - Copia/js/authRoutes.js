e323caecbf39784999e6a348e558fc9cd1585854
PedroMAnjos <pedromestredosanjos@gmail.com>
2026-05-18 03:54:02 -0300

    Delete projetos - Copia directory

--- projetos - Copia/js/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('./authController');
-const authMiddleware = require('./authMiddleware');
-
-router.post('/login', AuthController.login);
-router.post('/validate-hash', authMiddleware, AuthController.validateHash);
-
-module.exports = router;
