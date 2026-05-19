bda5eaf85970cd3d55d4fcf8874eff7c82ba7b18
gabriel-wav <gabfer.silva@gmail.com>
2026-05-18 21:57:51 -0300

    tentativa de correcao 2

---
+++ api/index.js
@@ -0,0 +1,4 @@
+const app = require('../projetos - Copia/js/server');
+
+// Exporta o app Express para ser processado nativamente pelo Vercel Serverless
+module.exports = app;
--- projetos - Copia/js/index.js
+++
@@ -1,4 +0,0 @@
-const app = require('./server');
-
-// Exporta o app Express para ser processado nativamente pelo Vercel Serverless
-module.exports = app;
